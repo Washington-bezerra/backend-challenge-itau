75fe0f4df719c080fc907544d570ba257d4ebb66
Washington Bezerra <luiswashington00@gmail.com>
2025-12-23 19:37:11 -0300

    feat: created exception advice

---
+++ src/main/kotlin/com/itau/password_validator/infrastructure/GlobalExceptionHandler.kt
@@ -0,0 +1,42 @@
+package com.itau.password_validator.infrastructure
+
+import com.itau.password_validator.infrastructure.v1.password.response.ValidatePasswordErrorResponse
+import org.apache.coyote.BadRequestException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ValidatePasswordErrorResponse> {
+        return ResponseEntity.badRequest().body(
+            ValidatePasswordErrorResponse(
+                code = 400,
+                message = e.bindingResult.fieldError?.defaultMessage ?: e.message
+            )
+        )
+    }
+
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(e: RuntimeException): ResponseEntity<ValidatePasswordErrorResponse> {
+        return ResponseEntity.internalServerError().body(
+            ValidatePasswordErrorResponse(
+                code = 400,
+                message = e.message?: "Unexpected error"
+            )
+        )
+    }
+
+    @ExceptionHandler(BadRequestException::class)
+    fun handleBadRequestException(e: BadRequestException): ResponseEntity<ValidatePasswordErrorResponse> {
+        return ResponseEntity.badRequest().body(
+            ValidatePasswordErrorResponse(
+                code = 400,
+                message = e.message?: "Unexpected error"
+            )
+        )
+    }
+}
