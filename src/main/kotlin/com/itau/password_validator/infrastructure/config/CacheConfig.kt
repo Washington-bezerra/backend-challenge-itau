47f6567c7ba1d927c7e1b7d3ea0c01a05432755b
Washington Bezerra <luiswashington00@gmail.com>
2025-12-28 17:54:03 -0300

    feat: added cache

--- src/main/kotlin/com/itau/password_validator/infrastructure/config/CacheConfig.kt
+++ src/main/kotlin/com/itau/password_validator/infrastructure/config/CacheConfig.kt
@@ -1,23 +1,15 @@
 package com.itau.password_validator.infrastructure.config
 
-import com.github.benmanes.caffeine.cache.Caffeine
 import org.springframework.cache.CacheManager
-import org.springframework.cache.caffeine.CaffeineCacheManager
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.time.Duration
 
 @Configuration
 class CacheConfig {
 
     @Bean
     fun cacheManager(): CacheManager {
-        val cacheManager = CaffeineCacheManager("passwordValidation")
-        cacheManager.setCaffeine(
-            Caffeine.newBuilder()
-                .expireAfterWrite(Duration.ofMinutes(30)) // TTL de 30 minutos
-                .maximumSize(1000)
-        )
-        return cacheManager
+        return ConcurrentMapCacheManager("passwordValidation")
     }
 }
